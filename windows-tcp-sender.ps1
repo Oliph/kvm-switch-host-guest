b12029da3efcaeac8b1ef80503387712770fe4e1
Oliph <olivier.philippe@gmail.com>
2017-01-26 21:16:14 +0000

    added windows-tcp-senders.ps1

---
+++ windows-tcp-sender.ps1
@@ -0,0 +1,42 @@
+﻿# This function is used to send a message throught TCP.
+# All credits to here: http://stackoverflow.com/documentation/powershell/5125/tcp-communication-with-powershell#t=20170126105250343293
+
+Function Send-TCPMessage { 
+    Param ( 
+            [Parameter(Mandatory=$true, Position=0)]
+            [ValidateNotNullOrEmpty()] 
+            [string] 
+            $EndPoint
+        , 
+            [Parameter(Mandatory=$true, Position=1)]
+            [int]
+            $Port
+        , 
+            [Parameter(Mandatory=$true, Position=2)]
+            [string]
+            $Message
+    ) 
+    Process {
+        # Setup connection 
+        $IP = [System.Net.Dns]::GetHostAddresses($EndPoint) 
+        $Address = [System.Net.IPAddress]::Parse($IP) 
+        $Socket = New-Object System.Net.Sockets.TCPClient($Address,$Port) 
+    
+        # Setup stream wrtier 
+        $Stream = $Socket.GetStream() 
+        $Writer = New-Object System.IO.StreamWriter($Stream)
+
+        # Write message to stream
+        $Message | % {
+            $Writer.WriteLine($_)
+            $Writer.Flush()
+        }
+    
+        # Close connection and stream
+        $Stream.Close()
+        $Socket.Close()
+    }
+}
+
+Sent-TCPMessage -Port $PORT -ENDPOINT 127.0.0.1 -message $KEYNAME
+Sent-TCPMessage -Port $PORT -ENDPOINT $HOSTIP -message $KEYNAME
