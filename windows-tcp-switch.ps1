c2499652ccb41c7a9772e69e62352615fde38465
Olivier PHILIPPE <olivier.philippe@gmail.com>
2017-01-26 15:46:31 +0000

    Add the windows-tcp-switch.ps1

---
+++ windows-tcp-switch.ps1
@@ -0,0 +1,15 @@
+# Powershell script to switch the screen/synergy/kb+mouse between the host os (linux) and guest os (windows) using a signal sent throught TCP
+
+
+## Read config file
+
+
+## Set up variable
+
+## Functions
+
+
+## Set up initial state to be sure the guest is the second computer
+
+
+## Main loop
