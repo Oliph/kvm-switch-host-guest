7d52b43dd2896ca8f7f0fa8c16784e585e95a44f
Olivier PHILIPPE <olivier.philippe@gmail.com>
2017-01-26 13:47:08 +0000

    Progression on the script itself

--- linux-tcp-switch.sh
+++ linux-tcp-switch.sh
@@ -5,6 +5,12 @@
 #Port to communicate
 PORT=29800
 
+# IP of the guest
+GUESTIP=192.168.1.87
+
+# KEY That is received through tcp to check it is the right one
+KEYTOCHANGE='DfxcwRE202dk45'
+
 # Screens to modify
 MAINSCREEN=HDMI1
 SECONDSCREEN=HDMI2
@@ -13,14 +19,15 @@ THIRDSCREEN=VGA1
 # Name of the windows guest for kvm
 KVMGUESTNAME=win-10
 # Name of arch name for synergy
-LINUXGUESTNAME=arch
+SYNERGYNAME=arch
 
 # Different usb devices to attach or detach 
-WIRELESSKEYBOARD=/home/olivier/config_files/kvm/wireless_keyboard.xml
-WIRELESSMOUSE=/home/olivier/config_files/kvm/wireless_mouse.xml
-M500KB=/home/olivier/config_files/kvm/m500.xml
-WIREDMOUSE=/home/olivier/config_files/kvm/wired_mouse.xml
-DUALSHOCK=/home/olivier/config_files/kvm/dual_shock.xml
+M500KB=./usb_xml/m500.xml
+WIREDMOUSE=./usb_xml/wired_mouse.xml
+DUALSHOCK=./usb_xml/dual_shock.xml
+
+# Synergy config file that has been configured separately
+SYNERGYCONF = "./synergy_conf/linux_synergy_server.conf"
 
 
 ## Functions
@@ -34,13 +41,16 @@ function main_computer {
 
     sleep 2
 
-    ## 2 Getting the keyboard and the mouse back
+    ## 2. Getting the keyboard and the mouse back
+
     # #http://rolandtapken.de/blog/2011-04/how-auto-hotplug-usb-devices-libvirt-vms-update-1
     echo "Doing the desactivation"
     virsh -c qemu:///system detach-device $KVMGUESTNAME $WIREDMOUSE || true 
     virsh -c qemu:///system detach-device $KVMGUESTNAME $M500KB || true
+    virsh -c qemu:///system detach-device $KVMGUESTNAME $DUALSHOCK  || true
 
-    # ## 4 Killing synergy clients
+    ## 3. Killing synergy clients
+
     ## Wait a little bit to have time to switch synergy on the host
     sleep 5
     echo "Killing synergy"
@@ -49,11 +59,13 @@ function main_computer {
     killall -9 synergy  || true
     # wait 2 second before launching synergy
     sleep 2
-    # # 5 launching synergy back
-    echo "Launching synergy"
-    synergy -c /home/olivier/config_files/kvm/synergy_server.conf &
 
-    exit
+    ## 4. Launching synergy back
+
+    echo "Launching synergy"
+    synergy -c $SYNERGYCONF &
+
+    # exit
 }
 
 
@@ -61,41 +73,66 @@ function second_computer {
     #### To make this computer the second one
 
     ## 1. Reset screen
+
     xrandr --output $SECONDSCREEN --primary --output $MAINSCREEN --off --output $THIRDSCREEN --off 
     sleep 2
-    ## 3 Getting the keyboard and the mouse back
+
+    ## 2. Getting the keyboard and the mouse back
+
     #http://rolandtapken.de/blog/2011-04/how-auto-hotplug-usb-devices-libvirt-vms-update-1
     virsh -c qemu:///system attach-device $KVMGUESTNAME $WIREDMOUSE  || true
     virsh -c qemu:///system attach-device $KVMGUESTNAME $M500KB || true
     virsh -c qemu:///system attach-device $KVMGUESTNAME $DUALSHOCK  || true
 
-    ## 4 Killing synergy clients
+    ## 2. Killing synergy clients
+
     killall -9 synergys  || true
     killall -9 synergyc  || true 
     killall -9 synergy  || true
     # Wait 2 sec before launching synergy
     sleep 2
-    # 5 launching synergy back
-    synergyc --name arch 192.168.1.87 &
-    exit
+
+    ## 3. Launching synergy back
+    synergyc --name $SYNERGYNAME $GUESTIP &
+    # exit
+}
+
+function test_func1 {
+    echo "test_func1"
+    echo $network_signal
+    # exit
+}
+
+function test_func2 {
+    echo "test_func2"
+    echo $network_signal
+    # exit
 }
 
 
 
 mode='main'
+
+## Main loop. Keep listening to the specified port and if receives the appropriate key, launch the
+## function to change between main and second computer 
 while true; 
 do
     # Using netcat to listen to the port. Close the connection as soon as getting a message
     # adding the option -k keeps it alive
     network_signal=`nc -l $PORT`
-    echo $network_signal
-    if [ "$mode" == 'main' ]; 
+    if [ "$network_signal" == $KEYTOCHANGE ];
     then
-        mode='second'
+        if [ "$mode" == 'main' ]; 
+        then
+            test_func1
+            mode="second"
+            
 
-    elif [ "$mode" == 'second' ];
-    then
-        mode='main'
+        elif [ "$mode" == 'second' ];
+        then
+            test_func2
+            mode="main"
+        fi
+        echo $mode
     fi
-    echo $mode
 done
